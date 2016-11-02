f660de8f1b62952d8c132225f11b46fc3dad50a8
randallerasmus <randallerasmus>
2016-11-02 15:02:05 +0200

    Update Controller and Views

---
+++ DreamTech/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DreamTech.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index(int? id)
+        {
+            var repo = new Repos.ProductRepo();
+
+            if (id == null)
+            {
+                var prodList = repo.GetAllProducts();
+                return View(prodList);
+            }
+            else
+            {
+                var prodList = repo.GetProductsByCategory(id);
+                return View(prodList);
+            }
+        }
+
+        public ActionResult SearchById()
+        {
+
+            return View();
+        }
+
+        public ActionResult NewProducts()
+        {
+            return View();
+        }
+
+        public ActionResult FeaturedProduct()
+        {
+            var repo = new Repos.FeaturedProductRepo();
+            var fProdList = repo.GetAllActiveFeaturedProducts();
+
+            return View(fProdList);
+        }
+    }
+}
