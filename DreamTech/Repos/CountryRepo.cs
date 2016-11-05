991e40c898ed96974a24a489e86802a504961915
Edmund Simons <edmundsimons69@gmail.com>
2016-11-05 12:52:38 +0200

    Add some repo's

---
+++ DreamTech/Repos/CountryRepo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using console.Models;
+
+namespace DreamTech.Repos
+{
+    public class CountryRepo
+    {
+        public List<console.Models.tblCountry> GetAllCountries()
+        {
+            using (var context = new console.Models.dream_techContext())
+            {
+                return context.tblCountries.Where(t => t.country_id > 0).ToList();
+            }
+        }
+
+        public bool SaveCountry(console.Models.tblCountry entity)
+        {
+            bool result = false;
+
+            using (var context = new console.Models.dream_techContext())
+            {
+                context.tblCountries.Add(entity);
+                result = context.SaveChanges() > 1;
+            }
+            return result;
+        }
+
+        public bool DeleteCountry(console.Models.tblCountry entity)
+        {
+            bool result = false;
+            using (var context = new console.Models.dream_techContext())
+            {
+                context.tblCountries.Remove(entity);
+                result = context.SaveChanges() > 1;
+            }
+            return result;
+        }
+    }
+}
---
+++ DreamTech/Repos/WhislistItemRepo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using console.Models;
+
+namespace DreamTech.Repos
+{
+    public class WhislistItemRepo
+    {
+        public List<console.Models.tbl_Wishlist> GetAllWishlists()
+        {
+            using (var context = new console.Models.dream_techContext())
+            {
+                return context.tbl_Wishlist.Where(t => t.wishlist_id > 0).ToList();
+            }
+        }
+
+        public bool SaveWishlist(console.Models.tbl_Wishlist entity)
+        {
+            bool result = false;
+
+            using (var context = new console.Models.dream_techContext())
+            {
+                context.tbl_Wishlist.Add(entity);
+                result = context.SaveChanges() > 1;
+            }
+            return result;
+        }
+
+        public tbl_WishlistItem GetWishlistItem(int id)
+        {
+            using (var context = new console.Models.dream_techContext())
+            {
+                return (from w in context.tbl_WishlistItem where w.wishlist_item_id == id select w).SingleOrDefault();
+            }
+        }
+
+        public bool UpdateWishlistItem(console.Models.tbl_WishlistItem entity)
+        {
+            bool result = false;
+            using (var context = new console.Models.dream_techContext())
+            {
+                tbl_WishlistItem wishlistItem = (from w in context.tbl_WishlistItem where w.wishlist_item_id == entity.wishlist_item_id select w).First();
+
+                wishlistItem.price_id = entity.price_id;
+                wishlistItem.product_id = entity.product_id;
+                wishlistItem.price_id = entity.price_id;
+
+                result = context.SaveChanges() > 1;
+            }
+            return result;
+        }
+
+        public bool DeleteWishlist(console.Models.tbl_Wishlist entity)
+        {
+            bool result = false;
+            using (var context = new console.Models.dream_techContext())
+            {
+                context.tbl_Wishlist.Remove(entity);
+                result = context.SaveChanges() > 1;
+            }
+            return result;
+        }
+    }
+}
