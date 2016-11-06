c6fe4b7d909424103d4a30626bf0eae2f06dd6ff
Ephraim <lebesee@gmail.com>
2016-11-06 12:28:47 -0800

    [Ephraim] Controller and views

---
+++ DreamTech/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DreamTech.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var repo = new Repos.CategoryRepo();
+            var categoryList = repo.GetAllCategories();
+
+            return View(categoryList);
+        }
+    }
+}
